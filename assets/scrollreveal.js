64b598a0c3df4f2074aef4a3efa259a006b8d85c
lgluiz1 <legalhints@gmail.com>
2023-02-23 02:14:46 -0300

    inclusao de js e versao mobile

---
+++ assets/scrollreveal.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal({ reset: true})
+
+ScrollReveal().reveal('.area-1', { 
+    duration: 1000, rotate:{
+        x:0,
+        y:80,
+        z:0
+    } });
+ScrollReveal().reveal('.area-2', { 
+    duration: 1000, rotate:{
+    x:0,
+    y:800,
+    z:00
+} });
+ScrollReveal().reveal('.area-3', { duration: 1000, 
+    rotate:{
+    x:100,
+    y:0,
+    z:30
+} });
